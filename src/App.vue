e9737e841f82f29ddc55deede70f2293bf9a0c33
Пузыно Эдуард <draconus72@gmail.com>
2021-07-22 18:18:42 +0300

    feature/TD-2: make form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="page">
+      <span>What needs to be done?</span>
+      <br />
+      <span>{{ checkedNames }}</span>
+      <br />
+      <label
+        ><input
+          type="checkbox"
+          id="one"
+          value="Learn React!"
+          v-model="checkedNames"
+        />Learn React!</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          id="two"
+          value="Make HomeWork!"
+          v-model="checkedNames"
+        />Make HomeWork!</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          id="three"
+          value="Relaxation!"
+          v-model="checkedNames"
+        />Relaxation!</label
+      ><br />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      checkedNames: [],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css">
+  .app {
+    display: flex;
+    margin: 30% auto;
+    background-color: #444;
+  }
+
+  .page {
+    display: flex;
+    flex-direction: column;
+    width: 31%;
+    height: 40%;
+    margin: 0.5% auto;
+    padding: 1rem;
+    background: #ccc;
+    border: 0.125rem solid #000;
+  }
+
+  span {
+    font-size: 2.25vw;
+    margin: 0% auto;
+  }
+
+  label {
+    font-size: 2.5vw;
+    vertical-align: middle;
+    margin: 1% 0.5%;
+  }
+
+  input {
+    transform: scale(1.75);
+    vertical-align: middle;
+    margin: 1%;
+  }
+
 </style>
