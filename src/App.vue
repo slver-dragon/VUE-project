ff2385401d31bf3a2916e1175462448f856c776b
Пузыно Эдуард <draconus72@gmail.com>
2021-07-29 14:46:38 +0300

    feature/TD-2: remake!

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,20 @@
 <template>
   <div id="app">
     <div class="page">
+      <span class="title block">to do list</span>
       <label class="element">
-        <span class="check-box"></span>
-        <input
-          class="input-point"
-          type="checkbox"
-          id="one"
-          value="Learn React!"
-          v-model="checkedNames"
-          checked
-        />Learn React!
+        <input type="checkbox" class="input-point" id="task-1" />
+        <span class="check-box"></span>Learn React!
       </label>
-      <br />
       <label class="element">
-        <span class="check-box"></span>
-        <input
-          class="input-point"
-          type="checkbox"
-          id="two"
-          value="Make HomeWork!"
-          v-model="checkedNames"
-        />Make HomeWork!
+        <input type="checkbox" class="input-point" id="task-2" />
+        <span class="check-box"></span>Make HomeWork!
       </label>
-      <br />
       <label class="element">
-        <span class="check-box"></span>
-        <input
-          class="input-point"
-          type="checkbox"
-          id="three"
-          value="Relaxation!"
-          v-model="checkedNames"
-        />Relaxation!
+        <input type="checkbox" class="input-point" id="task-3" />
+        <span class="check-box"></span>Relaxation!
       </label>
-      <br />
-      <span class="task-list">What needs to be done?</span>
-      <br />
-      <span class="task-list">{{ checkedNames }}</span>
+      <span class="footer block"></span>
     </div>
   </div>
 </template>
@@ -45,71 +22,124 @@
 <script>
 export default {
   data() {
-    return {
-      checkedNames: [],
-    };
+    return {};
   },
 };
 </script>
 
-<style lang="css">
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap");
+
+$myFont: "Open Sans";
+$border: 0.0625rem solid #000;
+$permission: 62rem,31rem;
+
 #app {
   display: flex;
   margin: auto;
-  background-color: #444;
+  background-color: #e5e5e5;
 }
 
 .page {
   display: flex;
   flex-direction: column;
-  width: 31%;
+  width: 21%;
   height: 40%;
   margin: 0.5% auto;
-  padding: 1rem;
   background: #ccc;
-  border: 0.125rem solid #000;
+  border: $border;
+
+  .block {
+    font-family: $myFont;
+    font-size: 1.25rem;
+    font-weight: 600;
+    text-align: center;
+    background: #aaa;
+  }
+
+  .title {
+    border-bottom: $border;
+  }
+
+  .element {
+    font-size: 1.25rem;
+    font-weight: 400;
+    margin: 1% 0.5%;
+    padding-left: 1.2em;
+    background-color: #bbb;
+    border-radius: 5%;
+
+    .input-point {
+      margin: 1%;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+    }
+
+    .check-box {
+      display: inline-block;
+      width: 0.5em;
+      height: 0.5em;
+      border: 0.1em solid black;
+      border-radius: 50%;
+      position: relative;
+      margin-right: 0.5em;
+    }
+
+    .check-box:before {
+      display: block;
+      content: "";
+      position: absolute;
+      width: 0.3em;
+      height: 0.3em;
+      top: 0.1em;
+      left: 0.1em;
+      background-color: black;
+      border-radius: 50%;
+      opacity: 0;
+    }
+
+    .input-point:checked + .check-box:before {
+      opacity: 1;
+    }
+
+    .footer {
+      border-top: $border;
+    }
+  }
 }
 
-.task-list {
-  font-size: 2.25vw;
-  margin: 0% auto;
+@media (max-width: nth($permission,1)) {
+  .page {
+    width: 50%;
+  }
 }
 
-.element {
-  font-size: 2.5vw;
-  margin: 1% 0.5%;
-  padding-left: 1.2em;
-}
-
-.input-point {
-  margin: 1%;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-}
-
-.check-box {
-  display: inline-block;
-  width: 0.6em;
-  height: 0.6em;
-  border: 0.1em solid black;
-  border-radius: 50%;
-  position: relative;
-}
-
-.check-box::before {
-  display: block;
-  content: "";
-  position: absolute;
-  width: 0.5em;
-  height: 0.5em;
-  top: 0.05em;
-  left: 0.05em;
-  background-color: black;
-  border-radius: 50%;
-  opacity: 0;
-}
-
-.input-point:checked + .check-box::before {
-  opacity: 1;
+@media (max-width: nth($permission,2)) {
+  .page {
+    width: 100%;
+  }
 }
 </style>
+
+<!--<div class="page">
+      <label class="element">
+        <span class="check-box"></span>
+        <input class="input-point" type="checkbox" id="one" value="Learn React!" checked />Learn React!
+      </label>
+      <br />
+      <label class="element">
+        <span class="check-box"></span>
+        <input class="input-point" type="checkbox" id="two" value="Make HomeWork!" />Make HomeWork!
+      </label>
+      <br />
+      <label class="element">
+        <span class="check-box"></span>
+        <input class="input-point" type="checkbox" id="three" value="Relaxation!" />Relaxation!
+      </label>
+      <br />
+      <span class="task-list">What needs to be done?</span>
+      <br />
+      <span class="task-list">{{ checkedNames }}</span>
+    </div>-->
+
+<!---->
