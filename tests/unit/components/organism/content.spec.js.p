d9ec40986c44b74191b339cd85049156a23b4af4
Пузыно Эдуард <draconus72@gmail.com>
2021-08-23 11:55:35 +0300

    feature/TD-6: unknown trouble

--- tests/unit/components/organism/content.spec.js.p
+++
@@ -1,6 +0,0 @@
-import { mount } from "@vue/test-utils";
-import newTask from "../../../../src/components/organism/Content";
-
-describe("content", () => {
-    
-  })
---
+++ tests/unit/store/index.spec.js
@@ -0,0 +1,47 @@
+import store from "../../../src/store/index.js";
+
+let taskData = [
+  {
+    id: 1,
+    task: "Task 1",
+    isChecked: false,
+  },
+  {
+    id: 2,
+    task: "Task 2",
+    isChecked: true,
+  },
+  {
+    id: 3,
+    task: "Task 3",
+    isChecked: false,
+  },
+];
+
+let sortData = [
+  {
+    id: 1,
+    type: "All",
+    isPicked: true,
+  },
+  {
+    id: 2,
+    type: "Active",
+    isPicked: false,
+  },
+  {
+    id: 3,
+    type: "Complete",
+    isPicked: false,
+  },
+];
+
+let state = { taskData };
+
+describe("task", () => {
+  it("возвращает задания", () => {
+    let current = store.getters.allTasks();
+    console.log(current);
+    expect(current).toEqual([ taskData[0], taskData[2] ])
+  });
+});
