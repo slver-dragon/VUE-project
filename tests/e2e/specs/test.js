c0da139fe32d7e194987962ef6643aebaa6a69b6
Пузыно Эдуард <draconus72@gmail.com>
2021-09-01 11:21:30 +0300

    feature/TD-7: e2e testing finished

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,20 @@
-// https://docs.cypress.io/api/introduction/api.html
+describe("Todo app e2e test", () => {
+  it("New task enter", () => {
+    cy.visit("localhost:8080");
+    cy.get('[placeholder="Add a new task"]').type('NewTask{Enter}');
+  });
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+  it("Change task status", () => {
+    cy.get('div').contains('NewTask').click();
+  });
+
+  it("Task sorting", () => {
+    cy.get('span').contains('Complete').click();
+    cy.get('span').contains('Active').click();
+    cy.get('span').contains('All').click();
+  });
+
+  it("New task delete", () => {
+    cy.get('div').contains('NewTask').siblings('div').click();
   });
 });
