03049035733dff5230f9f26178292d062d6e0754
Пузыно Эдуард <draconus72@gmail.com>
2022-02-11 12:22:55 +0300

    bugfix/main: resolve conflict

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div class="page">
+      <h1 class="title">Список задач</h1>
+      <Content />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Content from "./components/organism/Content.vue";
+import Footer from "./components/organism/Footer.vue";
+import { mapMutations } from "vuex";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Content,
+    Footer,
+  },
+
+  methods: {
+    ...mapMutations(["loadData"]),
+  },
+
+  mounted () {
+    this.loadData();
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/styles/style.scss";
+
+#app {
+  height: 100vh;
+  background: url(./assets/img/back.png) no-repeat center center;
+  background-size: cover;
+  padding-top: 5rem;
+
+  .page {
+    margin: 0 auto;
+    max-width: 33.75rem;
+    background: #fff4e9;
+    font-family: $myFontFamily;
+    color: $myFontColor;
+
+    .title {
+      text-align: center;
+      font-weight: 600;
+      background: #ffca93;
+      padding: 1rem 1.75rem;
+    }
+  }
+}
+</style>
